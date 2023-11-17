469946347cbec47dd59753480f713b088a8a6278
jiraporn-dada <jiraporn.boons@dome.tu.ac.th>
2023-11-17 21:39:43 +0700

    update rmse and mae

--- lossCal.py
+++ lossCal.py
@@ -1,35 +1,49 @@
 import mediapipe as mp
 import openpose as op
 import numpy as np
-
-#ใช้ library 
+import math
 def detect_coordinates():
-    x_coordinates = np.array([])#น่าจะเป็นพิกัด xของท่า
-    y_coordinates = np.array([])#น่าจะเป็นพิกัด yของท่า
+    x_coordinates = np.array([])  # น่าจะเป็นพิกัด x ของท่า
+    y_coordinates = np.array([])  # น่าจะเป็นพิกัด y ของท่า
     return x_coordinates, y_coordinates
 
-#เปรียบเทียบค่าที่ได้กับ correctCoord.txt
-def compare_coordinates(x_detected, y_detected, correct_coord_file):
-    #อ่านไฟล์ correctCoord.txt
+# เปรียบเทียบค่าที่ได้กับ correctCoord.txt
+def compare_coordinates(correct_coord_file):
+    # อ่านไฟล์ correctCoord.txt
     with open(correct_coord_file, 'r') as file:
         correct_coords = file.readlines()
 
     # แปลงข้อมูลจากไฟล์เป็นรายการของพิกัด x และ y
-    correct_x = [float(line.split()[0]) for line in correct_coords] #แปลงค่า x ที่อยู่ในไฟล์text
-    correct_y = [float(line.split()[1]) for line in correct_coords] #แปลงค่า y ที่อยู่ในไฟล์text
+    correct_x = [float(line.split()[0]) for line in correct_coords]  # แปลงค่า x ที่อยู่ในไฟล์ text
+    correct_y = [float(line.split()[1]) for line in correct_coords]  # แปลงค่า y ที่อยู่ในไฟล์ text
 
-    # เปรียบเทียบข้อมูลที่ได้กับข้อมูลถูกต้อง
-    diff_x = x_detected - np.array(correct_x)
-    diff_y = y_detected - np.array(correct_y)
+    correct_x = np.array(correct_x)
+    correct_y = np.array(correct_y)
 
-    return diff_x, diff_y
+    return correct_x, correct_y
+
+def mean_absolute_error(compare_coordinates, detect_coordinates):
+    n = len(compare_coordinates)
+    if n != len(detect_coordinates):
+        raise ValueError("จำนวนตัวอย่างไม่เท่ากัน")
+
+    mae = sum(abs(compare_coordinates[i] - detect_coordinates[i]) for i in range(n)) / n
+    return mae
+
+def root_mean_squared_error(compare_coordinates, detect_coordinates):
+    n = len(compare_coordinates)
+    if n != len(detect_coordinates):
+        raise ValueError("จำนวนตัวอย่างไม่เท่ากัน")
+
+    squared_errors = [(compare_coordinates[i] - detect_coordinates[i]) ** 2 for i in range(n)]
+    mean_squared_error = sum(squared_errors) / n
+    rmse = math.sqrt(mean_squared_error)
+    return rmse
 
 if __name__ == "__main__":
     x_detected, y_detected = detect_coordinates()
     correct_coord_file = "correctCoord.txt"
-    diff_x, diff_y = compare_coordinates(x_detected, y_detected, correct_coord_file)
+    correct_x, correct_y = compare_coordinates(correct_coord_file)
 
-    # วนลูปค่า x y ที่ได้มา
-    for i in range(len(diff_x)):
-        print(f"ความต่าง x ที่ได้ที่จุดที่ {i + 1}: {diff_x[i]}")
-        print(f"ความต่าง y ที่ได้ที่จุดที่ {i + 1}: {diff_y[i]}")
+    mae_result = mean_absolute_error(correct_x, x_detected)
+    print(f"Mean Absolute Error: {mae_result}")
